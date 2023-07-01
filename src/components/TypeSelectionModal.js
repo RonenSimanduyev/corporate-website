1764647a1be67cef2926329e97bbad3ee6b80fd3
ronensimanduyev <ronen.simanduyev1999@gmail.com>
2023-07-01 13:46:20 +0300

    added modal with images data

--- src/components/TypeSelectionModal.js
+++ src/components/TypeSelectionModal.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactModal from 'react-modal';
 
-export const TypeSelectionModal = ({ isOpen, onClose ,setCategory}) => {
+export const TypeSelectionModal = ({ isOpen, onClose, setCategory }) => {
     const handleTypeSelect = (type) => {
-        setCategory(type)
         onClose();
+        setCategory(type);
     };
-    let categoriesList=['animals','vacation','work']
+
+    let categoriesList = ['animals', 'vacation', 'work'];
+
     return (
         <ReactModal isOpen={isOpen} onRequestClose={onClose} contentLabel="Type Selection Modal">
-            <div className="text-black ">
+            <div className="text-black">
                 <h2 className="m-4">Select Type</h2>
                 {categoriesList.map((category) => (
-                    <button className="m-4 border-2 border-black p-3 w-20" key={category} onClick={() => handleTypeSelect(category)}>{category}</button>
-
+                    <button className="m-4 border-2 border-black p-3 w-20" key={category} onClick={() => handleTypeSelect(category)}>
+                        {category}
+                    </button>
                 ))}
             </div>
         </ReactModal>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,14 @@
-import {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import ReactModal from 'react-modal';
 import { TypeSelectionModal } from '../components/TypeSelectionModal';
 
-
-
 const HomePage = () => {
-    const [currentPage , setCurrentPage] =useState(1)
-    const [category , setCategory] =useState('')
+    const [currentPage, setCurrentPage] = useState(1);
+    const [category, setCategory] = useState('');
     const [isModalOpen, setIsModalOpen] = useState(false);
     const [selectedType, setSelectedType] = useState('');
+    const [selectedImage, setSelectedImage] = useState(null);
 
     const handleModalOpen = () => {
         setIsModalOpen(true);
@@ -25,6 +24,7 @@ const HomePage = () => {
         // Call server API or update state based on the selected type
         // You can add your logic here to fetch new data based on the selected type
     };
+
     const handlePrev = async () => {
         if (currentPage > 1) {
             const newPage = currentPage - 1;
@@ -47,9 +47,12 @@ const HomePage = () => {
         setCurrentPage(newPage);
     };
 
-
     const dispatch = useDispatch();
     const photos = useSelector((state) => state.photos.photos);
+
+    const handleImageSelect = (image) => {
+        setSelectedImage(image);
+    };
 
     useEffect(() => {
         const fetchData = async () => {
@@ -58,49 +61,98 @@ const HomePage = () => {
             );
             const data = await response.json();
             dispatch({ type: 'FETCH_PHOTOS', payload: data.hits.slice(0, 9) });
+            setCurrentPage(1)
         };
 
         fetchData();
     }, [category]);
+
     return (
         <div>
             <div className="items-center justify-center text-center">
-                <button className="items-center justify-center text-4xl m-2 rounded-2xl w-30 p-3 bg-[#ff9900]" onClick={handleModalOpen}>Select Category</button>
+                <button
+                    className="items-center justify-center text-4xl m-2 rounded-2xl w-30 p-3 bg-[#ff9900]"
+                    onClick={handleModalOpen}
+                >
+                    Select Category
+                </button>
             </div>
-            <TypeSelectionModal isOpen={isModalOpen} onClose={handleModalClose} onSelectType={handleTypeSelect} setCategory={setCategory} />
+            <TypeSelectionModal
+                isOpen={isModalOpen}
+                onClose={handleModalClose}
+                onSelectType={handleTypeSelect}
+                setCategory={setCategory}
+            />
 
             <div className="text-white text-4xl text-center mt-8 mb-4">
                 {category ? (
-                        <div>
-                            <h1>Category - {category}</h1>
+                    <div>
+                        <h1>Category - {category}</h1>
 
-                            <div className="grid grid-cols-3 gap-4">
-                                {photos.map((photo) => (
-                                    <div key={photo.id} className="m-5 flex items-center justify-center">
-                                        <img className="max-w-full max-h-full" src={photo.largeImageURL} alt={photo.title} />
-                                    </div>
-                                ))}
-                            </div>
-                            <div className="h-[180px] text-center sticky">
-                                <button className="items-center justify-center text-4xl m-2 rounded-2xl p-3 bg-[#ff9900]" onClick={handlePrev}>
-                                    prev
-                                </button>
-                                <span className="items-center justify-center text-4xl m-2 rounded-2xl py-3 px-5 bg-[#ff9900]">{currentPage}</span>
-                                <button className="items-center justify-center text-4xl m-2 rounded-2xl p-3 bg-[#ff9900]" onClick={handleNext}>
-                                    next
-                                </button>
-                            </div>
+                        <div className="grid grid-cols-3 gap-4">
+                            {photos.map((photo) => (
+                                <div
+                                    key={photo.id}
+                                    className="                  m-5 flex items-center justify-center"
+                                >
+                                    <img
+                                        className="max-w-full max-h-full cursor-pointer"
+                                        src={photo.largeImageURL}
+                                        alt={photo.title}
+                                        onClick={() => handleImageSelect(photo)}
+                                    />
+                                </div>
+                            ))}
                         </div>
+                        <ReactModal
+                            isOpen={selectedImage !== null}
+                            onRequestClose={() => setSelectedImage(null)}
+                            contentLabel="Image Details Modal"
+                        >
+                            {selectedImage && (
+                                <div>
+                                    <h2>Image Details</h2>
+                                    <img
+                                        className="w-[80vw] h-[30vw] items-center m-auto"
+                                        src={selectedImage.largeImageURL}
+                                        alt={selectedImage.title}
+                                    />
+                                    <div className="text-black flex items-center text-center justify-center">
+                                        <p className="m-3 border-2 border-black p-1 ">Views: {selectedImage.views}</p>
+                                        <p  className="m-3 border-2 border-black p-1 ">Downloads: {selectedImage.downloads}</p>
+                                        <p className="m-3 border-2 border-black p-1 ">Likes: {selectedImage.likes}</p>
+                                        <p className="m-3 border-2 border-black p-1 ">Tags: {selectedImage.tags}</p>
 
+                                    </div>
+
+                                </div>
+                            )}
+                        </ReactModal>
+                        <div className="h-[180px] text-center sticky">
+                            <button
+                                className="items-center justify-center text-4xl m-2 rounded-2xl p-3 bg-[#ff9900]"
+                                onClick={handlePrev}
+                            >
+                                prev
+                            </button>
+                            <span className="items-center justify-center text-4xl m-2 rounded-2xl py-3 px-5 bg-[#ff9900]">
+                {currentPage}
+              </span>
+                            <button
+                                className="items-center justify-center text-4xl m-2 rounded-2xl p-3 bg-[#ff9900]"
+                                onClick={handleNext}
+                            >
+                                next
+                            </button>
+                        </div>
+                    </div>
                 ) : (
                     <div>the category isn't selected, please choose one</div>
                 )}
             </div>
-
-
-
         </div>
     );
 };
 
 export default HomePage;
+
